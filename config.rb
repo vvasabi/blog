0ec732deadceefb0b2bb56213c4997037f74ffeb
Brad Chen <brad@bradchen.com>
2016-08-15 13:17:33 -0400

    It is now year 2016. Let's modernize the project setup a little bit.

--- config.rb
+++ config.rb
@@ -5,62 +5,75 @@
 # Time.zone = "UTC"
 
 activate :blog do |blog|
-  blog.prefix = "blog"
-  blog.permalink = ":year/:month/:title"
+  blog.prefix = 'blog'
+  blog.permalink = ':year/:month/:title'
   # blog.sources = ":year-:month-:day-:title.html"
   # blog.taglink = "tags/:tag.html"
-  blog.layout = "blog"
+  blog.layout = 'blog'
   # blog.summary_separator = /(READMORE)/
   # blog.summary_length = 250
   # blog.year_link = ":year.html"
   # blog.month_link = ":year/:month.html"
   # blog.day_link = ":year/:month/:day.html"
-  blog.default_extension = ".md"
-  blog.tag_template = "tag.html"
-  blog.calendar_template = "calendar.html"
+  blog.default_extension = '.md'
+  blog.tag_template = 'tag.html'
+  blog.calendar_template = 'calendar.html'
 
   blog.paginate = true
   blog.per_page = 6
   # blog.page_link = "page/:num"
 end
 
-page "/feed.xml", :layout => false
-page "/open-source-feed.xml", :layout => false
+page '/feed.xml', layout: false
+page '/open-source-feed.xml', layout: false
 
-### 
-# Compass
-###
-
-# Susy grids in Compass
-# First: gem install susy
-# require 'susy'
-
-# Change Compass configuration
-compass_config do |config|
-  config.output_style = :compressed
-end
-
+# Assets
+activate :sassc
 set :css_dir, 'css'
 set :js_dir, 'js'
 set :images_dir, 'images'
+set :font_dir, 'fonts'
+
+# Make external files available to Sprockets
+bower_path = File.join(root, 'bower_components')
+sprockets.append_path File.join(bower_path, 'bourbon/app/assets/stylesheets')
+sprockets.append_path bower_path
+
+bootstrap_path = File.join(bower_path, 'bootstrap-sass/assets')
+bootstrap_fonts_path = File.join(bootstrap_path, 'fonts')
+sprockets.append_path bootstrap_fonts_path
+sprockets.append_path File.join(bootstrap_path, 'javascripts')
+sprockets.append_path File.join(bootstrap_path, 'stylesheets')
+
+# Import Bootstrap fonts
+Dir.foreach(File.join(bootstrap_fonts_path, 'bootstrap')) do |file|
+  next if File.directory? File.join(bootstrap_fonts_path, 'bootstrap', file)
+  sprockets.import_asset File.join('bootstrap', file) do |path|
+    File.join(config[:font_dir], path)
+  end
+end
+
+# Do not output assets at wrong paths
+ignore 'fonts/bootstrap-sass/*'
+ignore 'images/bootstrap-sass/*'
 
 # Add syntax highlighting support
 require 'middleman-syntax'
 activate :syntax
 
 # Use GH style markdown
-set :markdown, :tables => true,
-    :autolink => true,
-    :gh_blockcode => true,
-    :fenced_code_blocks => true,
-    :smartypants => true
+set :markdown, tables: true,
+  autolink: true,
+  gh_blockcode: true,
+  fenced_code_blocks: true,
+  smartypants: true
 set :markdown_engine, :redcarpet
 
 configure :development do
   set :debug_assets, true
 
   # reload page
-  activate :livereload, :apply_js_live => true, :apply_css_live => true
+  activate :livereload
   config[:file_watcher_ignore] << %r{\.idea\/}
 end
 
@@ -86,4 +99,3 @@ configure :build do
   # Or use a different image path
   # set :http_path, "/Content/images/"
 end
-
--- source/meta.md
+++ source/meta.md
@@ -9,10 +9,15 @@ disable_comments: true
 See [this post](/blog/2012/12/migrating-from-drupal-to-middleman) for a list of
 tools used to build this site.
 
+**Update August 2016**: [Bower](https://bower.io) is used to manage JavaScript dependencies,
+[Bourbon](http://bourbon.io/) replaces [Compass](http://compass-style.org/), and
+[LibSass](http://sass-lang.com/libsass) replaces Ruby Sass.
+
 ### Fonts
 
-* This site uses [Myriad Pro](https://typekit.com/fonts/myriad-pro) through
-  [Typekit](https://typekit.com).
+* This site uses [Source Sans Pro](https://typekit.com/fonts/source-sans-pro),
+  [Source Code Pro](https://typekit.com/fonts/source-code-pro) and
+  [Myriad Pro](https://typekit.com/fonts/myriad-pro) through [Typekit](https://typekit.com).
 * This site uses icon fonts
   [Entypo pictograms by Daniel Bruce](http://www.entypo.com) and
   [IcoMoon by Keyamoon](http://icomoon.io/#icons). Both were prepared using
